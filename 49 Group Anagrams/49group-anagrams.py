34dbffa6b654cedef201e9562e05a0f8b9ff6440
rayxcast <rayxcast@gmail.com>
2026-04-02 21:47:07 +0000

    Code challenge submission for question: 49 Group Anagrams

--- 49 Group Anagrams/49group-anagrams.py
+++ 49 Group Anagrams/49group-anagrams.py
@@ -1,18 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        # Create a map which keys represent the characters as ID of each string in the array
-        anaMap = {}
-        def getStringId(s):
-            count = [0]*26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            return tuple(count)
+        ana_dic = {}
 
-        for s in strs:
-            sId = getStringId(s)
-            if sId not in anaMap:
-                anaMap[sId] = []
-            anaMap[sId].append(s)
+        for i in range(len(strs)):
+            key_ss = "".join(sorted(strs[i]))
+            if key_ss in ana_dic:
+                ana_dic[key_ss].append(strs[i])
+            else:
+                ana_dic[key_ss] = [strs[i]]
         
-        return [anaMap[key] for key in anaMap]
+        # print(ana_dic)
+        return [v for v in ana_dic.values()] # list(ana_dic.values()) 
