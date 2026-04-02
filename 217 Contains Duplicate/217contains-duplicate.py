b7b0842958552ac88ab0186b464a613357284e1c
rayxcast <rayxcast@gmail.com>
2026-04-02 20:57:15 +0000

    Code challenge submission for question: 217 Contains Duplicate

--- 217 Contains Duplicate/217contains-duplicate.py
+++ 217 Contains Duplicate/217contains-duplicate.py
@@ -1,38 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
-        # pre-conditions: given integer array
-        # post-contidions: return False if each value in the array is distint else return True
-
-        # Design idea:
-        # Using a for loop to check each integer in the array and using a set to check if the element exist
-
-        # Termination: after going through all the elements in the array
-
-        # if len(set(nums)) < len(nums):
-        #     return True
-        
-        # return False
-
-        # for i in nums:
-        #     if i not in s:
-        #         s.add(i)
-        #     else:
-        #         return True
-
-        # return False
-
-        hm = dict()
+        values_dic = {}
 
         for i in nums:
-            if i not in hm:
-                hm[i] = 1
+            if i in values_dic:
+                return True # return true since we just need any value to appear at least twice
             else:
-                return True
+                values_dic[i] = 1
         
         return False
-
-        # Memory complexity of at least O(n)
-        # Time complexiy is O(n)
-
-        
