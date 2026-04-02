609cc297c6635034aa69300ae5587edff57cf7fb
rayxcast <rayxcast@gmail.com>
2026-04-02 21:32:29 +0000

    Code challenge submission for question: 1 Two Sum

--- 1 Two Sum/1two-sum.py
+++ 1 Two Sum/1two-sum.py
@@ -1,15 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Let's create a set for fast checking
-        arrMap = {value: index for index, value in enumerate(nums)}
-        # print("arrMap:", arrMap)
-        output = []
-        for index, n in enumerate(nums):
-            # print("index, n:", index, n)
-            complement = target-n
-            # print("complement:", complement)
-            if complement in arrMap and arrMap[complement] != index:
-                output.extend([index, arrMap[complement]])
-                break
         
-        return output
+        nums_dic = {nums[i]: i for i in range(len(nums))}
+        for i in range(len(nums)):
+            c = target - nums[i]
+            if c in nums_dic and nums_dic[c] != i:
+                return [i, nums_dic[c]]
