54d277dd8f6087b55c3436ef9d7fbaf0ea8e6adb
rayxcast <rayxcast@gmail.com>
2026-04-08 23:37:51 +0000

    Code challenge submission for question: 20 Valid Parentheses

--- 20 Valid Parentheses/20valid-parentheses.py
+++ 20 Valid Parentheses/20valid-parentheses.py
@@ -1,31 +1,26 @@
-# What I can do here is just to remove all {}, (), [] and if the string is empty then it's a valid string
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s) <= 1:
+            return False
+             
+        close = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+
+        stack = []
+        for c in s:
+            if c in close and stack:
+                # print(c, stack[-1], close[c])
+                if stack[-1] == close[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
         
-        while ("()" in s) or ("[]" in s) or ("{}" in s):
-            s = s.replace("()","")
-            s = s.replace("[]","")
-            s = s.replace("{}","")
-        
-        if len(s) <= 0:
-            return True;
-        else:
-            return False;
-        
-#     def isValid(self, s: str) -> bool:
-        
-#         stack = []
-#         charMap = {")":"(", "}":"{", "]":"["}
-        
-#         for c in s:
-#             if c in charMap:
-#                 if stack and stack[-1] == charMap[c]:
-#                     stack.pop()
-#                 else:
-#                     return False
-#             else:
-#                 stack.append(c)
-        
-#         return True if not stack else False
-                
-        
+        # print(stack)
+
+        return True if len(stack) == 0 else False
+            
