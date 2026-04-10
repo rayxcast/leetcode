a7803a737b7b24296c39b0d84b9b43d4e3cd321a
rayxcast <rayxcast@gmail.com>
2026-04-10 22:39:43 +0000

    Code challenge submission for question: 3 Longest Substring Without Repeating Characters

--- 3 Longest Substring Without Repeating Characters/3longest-substring-without-repeating-characters.py
+++ 3 Longest Substring Without Repeating Characters/3longest-substring-without-repeating-characters.py
@@ -1,25 +1,20 @@
-# Let's have a function which iterate through the strign and save the substring until a
-# repeating character is found. The current substring will be saved in a variable "longestSub"
-# and another variable "currentSub" will be used to form a new substring when a repeated char is found
-# if len(currentSub) > len(longestSub): longestSub = currentSub
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        longestSub = ""
-        currentSub = ""
-        i = 0
-        c = 0
-        while i < len(s):
-            if s[i] not in currentSub:
-                currentSub += s[i]
-            else:
-                c+=1
-                i = c
-                currentSub = s[c] # reset the current substring
-            
-            if len(currentSub) > len(longestSub):
-                longestSub = currentSub
-            
-            i += 1
+        if not s:
+            return 0
 
-        return len(longestSub)
+        l = 0
+        curr_sub = set()
+        max_sub = 0
+
+        for r in range(len(s)):
+            while s[r] in curr_sub:
+                curr_sub.remove(s[l])
+                l+=1
+            curr_sub.add(s[r])
+            max_sub = max(max_sub, r-l+1)
+            r+=1
+        
+        # print(curr_sub)
+
+        return max_sub
