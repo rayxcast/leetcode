aadbadfd02133b555f5143e451eeaf8c6b967b49
rayxcast <rayxcast@gmail.com>
2026-04-17 22:06:49 +0000

    Code challenge submission for question: 104 Maximum Depth of Binary Tree

--- 104 Maximum Depth of Binary Tree/104maximum-depth-of-binary-tree.py
+++ 104 Maximum Depth of Binary Tree/104maximum-depth-of-binary-tree.py
@@ -6,9 +6,40 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
+        if not root:
             return 0
         
-
-        return max((1+self.maxDepth(root.left)),(1+self.maxDepth(root.right)))
+        # DFS
+        # def traverse(node):
+        #     if not node:
+        #         return 0
+        #     return 1 + max(traverse(node.left), traverse(node.right))
         
+        # return traverse(root)
+
+        # BFS
+        # level = 0
+        # dq = deque([root])
+        # while dq:
+        #     for i in range(len(dq)):
+        #         node = dq.popleft()
+        #         if node.left:
+        #             dq.append(node.left)
+        #         if node.right:
+        #             dq.append(node.right)
+        #     level += 1
+        
+        # return level
+
+        # iterative DFS
+        stack = [[root, 1]]
+        res = 1
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        
+        return res
