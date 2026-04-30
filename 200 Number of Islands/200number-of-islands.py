f180813591d5245e29755d4e1c397b8d322b6954
rayxcast <rayxcast@gmail.com>
2026-04-30 21:17:37 +0000

    Code challenge submission for question: 200 Number of Islands

--- 200 Number of Islands/200number-of-islands.py
+++ 200 Number of Islands/200number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
+        rows = len(grid)
+        cols = len(grid[0])
 
-        visited = set()
         islands = 0
 
-        def dfs(visited, graph, root):
-            if root not in visited: # if this node hasn't been visited
-                # print(visited)
-                visited.add(root)
-                # explore the neighbors of root in the matrix using a right, left, bottom, top, appraoch
-                if (root[1]+1) < len(grid[root[0]]) and graph[root[0]][root[1]+1] == "1": # if there is a right neighbour of 1
-                    dfs(visited, graph, (root[0],root[1]+1))
-                if (root[1]-1) >= 0 and graph[root[0]][root[1]-1] == "1": # if there is a left neighbour of 1
-                    dfs(visited, graph, (root[0],root[1]-1))
-                if (root[0]+1) < len(grid) and graph[root[0]+1][root[1]] == "1": # if there is a bottom neighbour of 1
-                    dfs(visited, graph, (root[0]+1,root[1]))
-                if (root[0]-1) >= 0 and graph[root[0]-1][root[1]] == "1": # if there is a top neighbour of 1
-                    dfs(visited, graph, (root[0]-1,root[1]))
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                # print((i, j))
-                if (i, j) not in visited and grid[i][j] == "1":
-                    islands += 1
-                    dfs(visited, grid, (i, j))
+            grid[r][c] = "0"
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
         
-        # print(visited)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    islands+=1
+                    dfs(r, c)
+        
         return islands
-
-        
